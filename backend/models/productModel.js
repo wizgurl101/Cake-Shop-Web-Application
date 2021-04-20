3e6cbdcc836d339192c2b9538de1a71752a736f2
Nhu B. Phan <612600@ACDM.DS.SAIT.CA>
2021-04-19 21:30:17 -0600

    add home screen jpg and modified productModel.js to add rating object to product schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,24 +1,47 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const productSchema = mongoose.Schema({
+/**
+ * Product Schema
+ */
+const productSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     image: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     category: {
-        type: String,
-        required: true
-    }
-})
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Product = mongoose.model('Product', productSchema)
+const Product = mongoose.model('Product', productSchema);
 
-export default Product
+export default Product;
