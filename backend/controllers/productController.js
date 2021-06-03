1ea8bb904bcc609bc5e8177c7e5484c2ce3fa46d
wizgurl101 <nb.phan@hotmail.com>
2021-06-03 14:31:48 -0600

    implement get a single product by id function in backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,5 @@
-import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
 
 /**
  * @desc Fetch all products from database
@@ -9,6 +9,23 @@ import Product from '../models/productModel.js';
 const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find({});
   res.json({ products });
+});
+
+/**
+ * @desc Fetch a single product from database
+ * @route GET /cakeshop/products/:id
+ * @access Public
+ */
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  // if product exist
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found.");
+  }
 });
 
 /**
@@ -22,12 +39,12 @@ const deleteProduct = asyncHandler(async (req, res) => {
   // if product does exist, delete it from database
   if (productToDelete) {
     await productToDelete.remove();
-    res.json({ message: 'Product deleted.' });
+    res.json({ message: "Product deleted." });
   } else {
     // product does not exist
     res.status(404);
-    throw new Error('No such product exist.');
+    throw new Error("No such product exist.");
   }
 });
 
-export { getProducts, deleteProduct };
+export { getProducts, getProductById, deleteProduct };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,19 @@
-import express from 'express';
+import express from "express";
 import {
   getProducts,
+  getProductById,
   deleteProduct,
-} from '../controllers/productController.js';
+} from "../controllers/productController.js";
 
-import { protect, isAdmin } from '../middleware/authMiddleware.js';
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route('/').get(getProducts);
+router.route("/").get(getProducts);
 
-router.route('/:id').delete(protect, isAdmin, deleteProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, isAdmin, deleteProduct);
 
 export default router;
