c64f09abd44c36c7782f2ca05551eb1c7cb7d962
wizgurl101 <nb.phan@hotmail.com>
2021-06-03 09:36:44 -0600

    product list screen list all products for admin user

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,44 +1,43 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
 
-import Product from '../components/Product'
-import Loader from '../components/Loader'
-import Message from '../components/Message'
-import { listProducts } from '../actions/productActions'
+import Product from "../components/Product";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const productList = useSelector(state => state.productList)
-    const { loading, error, products } = productList
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
-    console.log('products is')
-    console.log(products)
+  // console.log('products is')
+  // console.log(products)
 
-    useEffect(() => {
-        dispatch(listProducts())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-    return (
-        <>
-           <h2>Latest Baked Goods</h2>
-            { loading ? (
-                <Loader/>
-                ) : error ? (
-                    <Message variant='danger'>{error}</Message> 
-                ) : (
-                    <Row>
-                        {products.map(product => (
-                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                                <Product product={product}/>
-                            </Col>
-                        ))}
-                    </Row>
-                )
-            }
-        </>
-    )
-}
+  return (
+    <>
+      <h2>Latest Baked Goods</h2>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -1,12 +1,12 @@
-import { useEffect } from 'react';
-import { LinkContainer } from 'react-router-bootstrap';
-import { Table, Button, Row, Col } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import Message from '../components/Message.js';
-import Loader from '../components/Loader.js';
-import { listProducts } from '../actions/productActions.js';
+import { useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message.js";
+import Loader from "../components/Loader.js";
+import { listProducts } from "../actions/productActions.js";
 
-const ProductListScreen = () => {
+const ProductListScreen = ({ history }) => {
   const dispatch = useDispatch();
 
   // get all products
@@ -14,34 +14,34 @@ const ProductListScreen = () => {
   const { loading, error, products } = productList;
 
   // get current log in user info
-  const currentUser = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
+  const currentUserLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = currentUserLogin;
 
   useEffect(() => {
     // if current user is not admin, redirect to login page
     if (!userInfo.isAdmin) {
-      history.pushState('/login');
+      history.pushState("/login");
     }
   }, [userInfo, history]);
 
   // FUNCTIONS
   const deleteProductHander = () => {
-    console.log('deleted product');
+    console.log("deleted product");
   };
 
   const createNewProductHandler = () => {
-    console.log('add new product');
+    console.log("add new product");
   };
 
   return (
     <>
-      <Row className='align-items-center'>
+      <Row className="align-items-center">
         <Col>
           <h1>Products</h1>
         </Col>
-        <Col className='text-right'>
-          <Button className='my-3' onClick={createNewProductHandler}>
-            <i className='fas fa-plus' />
+        <Col className="text-right">
+          <Button className="my-3" onClick={createNewProductHandler}>
+            <i className="fas fa-plus" />
             Add new product
           </Button>
         </Col>
@@ -49,10 +49,10 @@ const ProductListScreen = () => {
       {loading ? (
         <Loader />
       ) : error ? (
-        <Message variant='danger'>{error}</Message>
+        <Message variant="danger">{error}</Message>
       ) : (
         <>
-          <Table striped bordered hover responsive className='table-sm'>
+          <Table striped bordered hover responsive className="table-sm">
             <thead>
               <tr>
                 <th>PRODUCT ID</th>
@@ -69,16 +69,16 @@ const ProductListScreen = () => {
                   <td>{product.category}</td>
                   <td>
                     <LinkContainer to={`/admin/product/${product._id}/edit`}>
-                      <Button variant='light' className='btn-sm'>
-                        <i className='fas fa-edit' />
+                      <Button variant="light" className="btn-sm">
+                        <i className="fas fa-edit" />
                       </Button>
                     </LinkContainer>
                     <Button
-                      variant='danger'
-                      className='btn-sm'
+                      variant="danger"
+                      className="btn-sm"
                       onClick={() => deleteProductHander(product._id)}
                     >
-                      <i className='fas fa-trash' />
+                      <i className="fas fa-trash" />
                     </Button>
                   </td>
                 </tr>
