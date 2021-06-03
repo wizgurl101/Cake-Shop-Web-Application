510031cf637ee3bb5fe7b2a4463c53c226ade7a1
wizgurl101 <nb.phan@hotmail.com>
2021-06-03 15:13:40 -0600

    add constants, action and reducer for deleting a product in frontend

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,42 +1,44 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import {
-    productListReducer
-} from './reducers/productReducers.js'
+  productDeleteReducer,
+  productListReducer,
+} from "./reducers/productReducers.js";
 
-import { 
-    userLoginReducer, 
-    userRegisterReducer,
-    userListReducer,
-    userDeleteReducer, 
-    userUpdateReducer,
-    userDetailsReducer
-} from './reducers/userReducers.js'
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userListReducer,
+  userDeleteReducer,
+  userUpdateReducer,
+  userDetailsReducer,
+} from "./reducers/userReducers.js";
 
 const reducer = combineReducers({
-    productList: productListReducer,
-    userLogin: userLoginReducer,
-    userRegister: userRegisterReducer,
-    userList: userListReducer,
-    userDelete: userDeleteReducer,
-    userUpdate: userUpdateReducer,
-    userDetails: userDetailsReducer
-})
+  productList: productListReducer,
+  productDelete: productDeleteReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
+  userDetails: userDetailsReducer,
+});
 
-const userInfoFromStorage = localStorage.getItem('userInfo') 
-    ? JSON.parse(localStorage.getItem('userInfo'))
-    : null
-    
-const initialState = { 
-    userLogin: { userInfo: userInfoFromStorage }
-}
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromStorage },
+};
 
 const store = createStore(
-    reducer, 
-    initialState, 
-    composeWithDevTools(applyMiddleware(thunk))
-)
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
-export default store
+export default store;
