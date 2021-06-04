3d832bf9d68b9fc72cb0ba2c12eaaa606653a575
wizgurl101 <nb.phan@hotmail.com>
2021-06-04 13:10:55 -0600

    add basic form on product edit screen

--- frontend/src/screens/productScreen/ProductEditScreen.js
+++ frontend/src/screens/productScreen/ProductEditScreen.js
@@ -1,14 +1,64 @@
-// import axios from "axios";
-// import { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-// import { Form, Button } from "react-bootstrap";
-// import { useDispatch, useSelector } from "react-redux";
-// import Message from "../../components/Message";
-// import Loader from "../../components/Loader";
-// import FormContainer from "../../components/FormContainer";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import FormContainer from "../../components/FormContainer";
 
-const ProductEditScreen = () => {
-  return <div>Product Edit Screen</div>;
+const ProductEditScreen = ({ match, history }) => {
+  const dispatch = useDispatch();
+  const productId = match.params.id;
+
+  // product state variables
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+  const [uploading, setUploading] = useState(false);
+
+  // get product details
+  //   const productDetails = useSelector((state) => state.productDetail);
+  //   const { loading, error, product } = productDetails;
+
+  // update product
+  const productUpdated = useSelector((state) => state.productUpdate);
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = productUpdated;
+
+  // FUNCTIONS
+  const uploadImageFileHandler = () => {
+    console.log("image uploaded");
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("submitted");
+  };
+
+  return (
+    <>
+      <Link to="/admin/productlist" className="btn btn-light my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}></Form>
+        )}
+      </FormContainer>
+    </>
+  );
 };
 
 export default ProductEditScreen;
