2343532bc526df18adf9ca5b33de13c84f1a9cab
wizgurl101 <nb.phan@hotmail.com>
2021-07-05 20:15:18 -0600

    add cart items and shipping address to local storage

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -37,11 +37,23 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
 });
 
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItem"))
+  : [];
+
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : [];
+
 const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
 const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
