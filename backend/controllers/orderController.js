2b7e2fb682d735b0d0a5341a39d0a53281c3224f
wizgurl101 <nb.phan@hotmail.com>
2021-07-10 16:08:19 -0600

    add functions to update order when paid and delivered, get all orders, get all user orders and get order by id.  Add routes for them in backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -42,4 +42,103 @@ const createOrder = asyncHandler(async (req, res) => {
   }
 });
 
-export { createOrder };
+/**
+ * @desc Get order by ID
+ * @route GET /cakeshop/orders/:id
+ * @access Private
+ */
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  // check if order exist
+  if (order) {
+    res.json(order);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+/**
+ * @desc Update order to paid when payment was successful
+ * @route GET /cakeshop/orders/:id/paid
+ * @access Private
+ */
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  // if order exist, update isPaid field
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+
+    // get payment result from API i.e. PayPal
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const paidOrder = await order.save();
+    res.json(paidOrder);
+  } else {
+    // order was not found
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+/**
+ * @desc Update order to delivered
+ * @route GET /cakeshop/orders/:id/deliver
+ * @access Private
+ */
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  // if order exist, update it to delivered
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+
+    const deliveredOrder = await order.save();
+    res.json(deliveredOrder);
+  } else {
+    // order was not found
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
+/**
+ * @desc Update order to delivered
+ * @route GET /cakeshop/orders/userorders
+ * @access Private
+ */
+const getUserOrders = asyncHandler(async (req, res) => {
+  const userOrders = await Order.find({ user: req.user._id });
+  res.json(userOrders);
+});
+
+/**
+ * @desc Get all orders
+ * @route GET /cakeshop/orders/orderlist
+ * @access Private/Admin
+ */
+const getOrders = asyncHandler(async (req, res) => {
+  const allOrders = await Order.find({}).populate("user", "id name");
+  res.json(allOrders);
+});
+
+export {
+  createOrder,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getUserOrders,
+  getOrders,
+};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,20 @@
 import express from "express";
-import { createOrder } from "../controllers/orderController.js";
+import {
+  createOrder,
+  getOrderById,
+  getOrders,
+  getUserOrders,
+  updateOrderToDelivered,
+  updateOrderToPaid,
+} from "../controllers/orderController.js";
 import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(protect, createOrder);
+router.route("/").post(protect, createOrder).get(protect, isAdmin, getOrders);
+router.route("/userorders").get(protect, getUserOrders);
+router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
+router.route("/:id/deliver").put(protect, isAdmin, updateOrderToDelivered);
 
 export default router;
