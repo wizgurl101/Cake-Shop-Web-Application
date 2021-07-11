c5be5eaba7707eea3b5ae97f3c27dd4441a49d19
wizgurl101 <nb.phan@hotmail.com>
2021-07-11 15:03:42 -0600

    provide default value for input in forms to make shipping screen component controlled. Add Payment screen of the checkout process

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import RegisterScreen from "./screens/RegisterScreen";
 import CartScreen from "./screens/CartScreen";
 import ShippingScreen from "./screens/checkoutScreen/ShippingScreen";
 import DeliveryScreen from "./screens/checkoutScreen/DeliveryScreen";
+import PaymentScreen from "./screens/checkoutScreen/PaymentScreen";
 import UserListScreen from "./screens/userScreen/UserListScreen";
 import UserEditScreen from "./screens/userScreen/UserEditScreen";
 import ProfileScreen from "./screens/userScreen/ProfileScreen";
@@ -28,6 +29,7 @@ const App = () => {
           <Route path="/cart/:id" component={CartScreen} />
           <Route path="/shipping" component={ShippingScreen} />
           <Route path="/delivery" component={DeliveryScreen} />
+          <Route path="/payment" component={PaymentScreen} />
           <Route path="/product/:id" component={ProductDetailScreen} />
           <Route path="/admin/userList" component={UserListScreen} />
           <Route path="/admin/user/:id/edit" component={UserEditScreen} />
