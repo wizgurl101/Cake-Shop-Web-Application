7de8acb46a0bc84f4caa27cc1959719e51244b1f
wizgurl101 <nb.phan@hotmail.com>
2021-07-11 15:06:41 -0600

    remove cartItems, shipping address, delivery date and payment method from local storage when user logout.  Add code to redirect logout user to log in page in user actions

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,203 +1,241 @@
-import axios from 'axios'
-import { 
-    USER_LOGIN_REQUEST,
-    USER_LOGIN_SUCCESS,
-    USER_LOGIN_FAIL, 
-    USER_REGISTER_REQUEST,
-    USER_REGISTER_SUCCESS,
-    USER_REGISTER_FAIL,
-    USER_LOGOUT,
-    USER_LIST_REQUEST,
-    USER_LIST_SUCCESS,
-    USER_LIST_FAIL,
-    USER_DELETE_REQUEST,
-    USER_DELETE_SUCCESS,
-    USER_DELETE_FAIL,
-    USER_UPDATE_FAIL,
-    USER_UPDATE_REQUEST,
-    USER_UPDATE_SUCCESS,
-    USER_DETAILS_RESET,
-    USER_DETAILS_FAIL,
-    USER_DETAILS_SUCCESS,
-    USER_LIST_RESET,
-    USER_DETAILS_REQUEST
-} from '../constants/userConstants'
-
+import axios from "axios";
+import {
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCESS,
+  USER_LOGIN_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
+  USER_LOGOUT,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
+  USER_DELETE_REQUEST,
+  USER_DELETE_SUCCESS,
+  USER_DELETE_FAIL,
+  USER_UPDATE_FAIL,
+  USER_UPDATE_REQUEST,
+  USER_UPDATE_SUCCESS,
+  USER_DETAILS_RESET,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_SUCCESS,
+  USER_LIST_RESET,
+  USER_DETAILS_REQUEST,
+} from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
-    try {
-        dispatch({ type: USER_LOGIN_REQUEST })
+  try {
+    dispatch({ type: USER_LOGIN_REQUEST });
 
-        const config = {
-            headers: { 'Content-type': 'application/json' }
-        }
+    const config = {
+      headers: { "Content-type": "application/json" },
+    };
 
-        const { data } = await axios.post('/cakeshop/users/login', { email, password }, config)
+    const { data } = await axios.post(
+      "/cakeshop/users/login",
+      { email, password },
+      config
+    );
 
-        dispatch({
-            type: USER_LOGIN_SUCCESS,
-            payload: data
-        })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
 
-        localStorage.setItem('userInfo', JSON.stringify(data))
-
-    } catch (error) {
-        dispatch({
-            type: USER_LOGIN_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message
-        })
-    }
-}
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 // register a new user account
 export const register = (name, email, password) => async (dispatch) => {
-    try {
-        dispatch({ type: USER_REGISTER_REQUEST })
+  try {
+    dispatch({ type: USER_REGISTER_REQUEST });
 
-        const config = ({
-            headers: { 'Content-Type': 'application/json' }
-        })
+    const config = {
+      headers: { "Content-Type": "application/json" },
+    };
 
-        const { data } = await axios.post('/cakeshop/users', { name, email, password }, config)
+    const { data } = await axios.post(
+      "/cakeshop/users",
+      { name, email, password },
+      config
+    );
 
-        dispatch({
-            type: USER_REGISTER_SUCCESS,
-            payload: data 
-        })
+    dispatch({
+      type: USER_REGISTER_SUCCESS,
+      payload: data,
+    });
 
-        dispatch({
-            type: USER_LOGIN_SUCCESS,
-            payload: data
-        })
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
 
-        localStorage.setItem('userInfo', JSON.stringify(data))
-    } catch (error) {
-        dispatch({
-            type: USER_REGISTER_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message
-        })
-    }
-}
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 export const logout = () => (dispatch) => {
-    localStorage.removeItem('userInfo')
-    dispatch({ type: USER_LOGOUT })
-    dispatch({ type: USER_DETAILS_RESET })
-    dispatch({ type: USER_LIST_RESET })
-}
+  localStorage.removeItem("userInfo");
+  localStorage.removeItem("cartItems");
+  localStorage.removeItem("shippingAddress");
+  localStorage.removeItem("deliveryDate");
+  localStorage.removeItem("paymentMethod");
+  dispatch({ type: USER_LOGOUT });
+  dispatch({ type: USER_DETAILS_RESET });
+  dispatch({ type: USER_LIST_RESET });
+  document.location.href = "/login";
+};
 
 export const listUsers = () => async (dispatch, getState) => {
-    try {
-        dispatch({ type: USER_LIST_REQUEST })
+  try {
+    dispatch({ type: USER_LIST_REQUEST });
 
-        const { userLogin: { userInfo } } = getState()
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
-        console.log('listUsers method, getState is')
-        console.log(getState())
+    console.log("listUsers method, getState is");
+    console.log(getState());
 
-        const config = {
-            headers: { Authorization: `Bearer ${userInfo.token}` }
-        }
+    const config = {
+      headers: { Authorization: `Bearer ${userInfo.token}` },
+    };
 
-        const { data } = await axios.get(`/cakeshop/users`, config)
+    const { data } = await axios.get(`/cakeshop/users`, config);
 
-        dispatch({
-            type: USER_LIST_SUCCESS,
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: USER_LIST_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message
-        })
-    }
-}
+    dispatch({
+      type: USER_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 // delete a user
 export const deleteUser = (id) => async (dispatch, getState) => {
-    try {
-        dispatch({ type: USER_DELETE_REQUEST })
+  try {
+    dispatch({ type: USER_DELETE_REQUEST });
 
-        // get user info from the state
-        const { userLogin: { userInfo } } = getState()
+    // get user info from the state
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
-        // need to send authorization token in the headers
-        const config = {
-            headers: {
-                Authorization: `Bearer ${userInfo.token}`
-            }
-        }
+    // need to send authorization token in the headers
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
 
-        // delete the user from database
-        await axios.delete(`/cakeshop/users/${id}`, config)
+    // delete the user from database
+    await axios.delete(`/cakeshop/users/${id}`, config);
 
-        dispatch({ type: USER_DELETE_SUCCESS })
-
-    } catch (error) {
-        dispatch({
-            type: USER_DELETE_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message
-        })
-    }
-}
+    dispatch({ type: USER_DELETE_SUCCESS });
+  } catch (error) {
+    dispatch({
+      type: USER_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 // update user information from admin user list screen
 export const updateUser = (user) => async (dispatch, getState) => {
-    try {
-        dispatch({ type: USER_UPDATE_REQUEST })
+  try {
+    dispatch({ type: USER_UPDATE_REQUEST });
 
-        const { userLogin: { userInfo } } = getState()
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${userInfo.token}`
-            }
-        }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
 
-        // get the user back with the updated information
-        const { data } = await axios.put(`/cakeshop/users/${user._id}`, user, config)
+    // get the user back with the updated information
+    const { data } = await axios.put(
+      `/cakeshop/users/${user._id}`,
+      user,
+      config
+    );
 
-        dispatch({ type: USER_UPDATE_SUCCESS })
+    dispatch({ type: USER_UPDATE_SUCCESS });
 
-        dispatch({
-            type: USER_DETAILS_RESET,
-            payload: data
-        })
-
-    } catch (error) {
-        dispatch({
-            type: USER_UPDATE_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message
-        })
-    }
-}
+    dispatch({
+      type: USER_DETAILS_RESET,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_UPDATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
 
 // get user details, called getUserById in backend
 export const getUserDetails = (id) => async (dispatch, getState) => {
-    try {
-        dispatch({ type: USER_DETAILS_REQUEST })
+  try {
+    dispatch({ type: USER_DETAILS_REQUEST });
 
-        const { userLogin: { userInfo } } = getState()
+    const {
+      userLogin: { userInfo },
+    } = getState();
 
-        const config = {
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${userInfo.token}`
-            }
-        }
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
 
-        const { data } = await axios.get(`/cakeshop/users/${id}`, config)
+    const { data } = await axios.get(`/cakeshop/users/${id}`, config);
 
-        dispatch({
-            type: USER_DETAILS_SUCCESS,
-            payload: data
-        })
-
-    } catch (error) {
-        dispatch({
-            type: USER_DETAILS_FAIL,
-            payload: error.response && error.response.data.message ? error.response.data.message : error.message
-        })
-    }
-}
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
