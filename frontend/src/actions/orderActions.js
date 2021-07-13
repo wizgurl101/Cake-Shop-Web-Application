d3de45200ddda71d6504c85696dd1b9455e9b2bf
wizgurl101 <nb.phan@hotmail.com>
2021-07-13 06:31:02 -0600

    add constants, action and reducer functions for listing user orders and all orders for admin

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -7,6 +7,12 @@ import {
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
+  ORDER_LIST_ADMIN_FAIL,
+  ORDER_LIST_ADMIN_REQUEST,
+  ORDER_LIST_ADMIN_SUCCESS,
+  ORDER_LIST_USER_FAIL,
+  ORDER_LIST_USER_REQUEST,
+  ORDER_LIST_USER_SUCCESS,
 } from "../constants/orderConstants";
 
 export const createOrder = (newOrder) => async (dispatch, getState) => {
@@ -59,6 +65,62 @@ export const getOrderDetails = (orderId) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: ORDER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listUserOrders = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_LIST_USER_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get("/cakeshop/orders/userorders", config);
+
+    dispatch({ type: ORDER_LIST_USER_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_USER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listAllOrders = () => async (dispatch, getState) => {
+  try {
+    dispatch({ type: ORDER_LIST_ADMIN_REQUEST });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get("/cakeshop/orders", config);
+
+    dispatch({ type: ORDER_LIST_ADMIN_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_ADMIN_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -5,6 +5,13 @@ import {
   ORDER_DETAILS_FAIL,
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
+  ORDER_LIST_ADMIN_FAIL,
+  ORDER_LIST_ADMIN_REQUEST,
+  ORDER_LIST_ADMIN_SUCCESS,
+  ORDER_LIST_USER_FAIL,
+  ORDER_LIST_USER_REQUEST,
+  ORDER_LIST_USER_RESET,
+  ORDER_LIST_USER_SUCCESS,
 } from "../constants/orderConstants";
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -34,6 +41,34 @@ export const orderDetailsReducer = (
     case ORDER_DETAILS_SUCCESS:
       return { loading: false, order: action.payload };
     case ORDER_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const orderListUserReducer = (state = { orders: [] }, action) => {
+  switch (action.type) {
+    case ORDER_LIST_USER_REQUEST:
+      return { loading: true };
+    case ORDER_LIST_USER_SUCCESS:
+      return { loading: false, orders: action.payload };
+    case ORDER_LIST_USER_FAIL:
+      return { loading: false, error: action.payload };
+    case ORDER_LIST_USER_RESET:
+      return { orders: [] };
+    default:
+      return state;
+  }
+};
+
+export const orderListAdminReducer = (state = { orders: [] }, action) => {
+  switch (action.type) {
+    case ORDER_LIST_ADMIN_REQUEST:
+      return { loading: true };
+    case ORDER_LIST_ADMIN_SUCCESS:
+      return { loading: false, orders: action.payload };
+    case ORDER_LIST_ADMIN_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
